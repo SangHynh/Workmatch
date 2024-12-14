89b450eac2b54f2bd2b8245e487428cc44c03a1d
SangHynh <huynhvansang.020102@gmail.com>
2024-12-14 13:07:26 +0700

    feat(ui): display username

--- src/main/java/springboot/controllers/admin/AdminController.java
+++ src/main/java/springboot/controllers/admin/AdminController.java
@@ -12,25 +12,30 @@ import springboot.controllers.BaseController;
 public class AdminController extends BaseController {
 
     @RequestMapping("/admin")
-    public String admin() {
+    public String admin(Model model) {
+        addUserEmailToModel(model);
         return "admin/index";
     }
 
     /* Job management */
     @RequestMapping("/admin/job")
-    public String job() {
+    public String job(Model model) {
+        addUserEmailToModel(model);
         return "admin/pages/job";
     }
 
     /* Company management */
     @RequestMapping("/admin/company")
-    public String company() {
+    public String company(Model model) {
+        addUserEmailToModel(model);
         return "admin/pages/company";
     }
 
     /* Candidate management */
     @RequestMapping("/admin/candidate")
-    public String candidate() {
+    public String candidate(Model model)
+    {
+        addUserEmailToModel(model);
         return "admin/pages/candidate";
     }
 
